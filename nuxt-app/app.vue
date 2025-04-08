6f19edb8592b3af1fce643f52dd6d7cd45da3777
Wang <740488087@qq.com>
2025-04-08 22:28:49 +0800

    feat:引入naive-ui并使用

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    npm i -D naive-ui
+    <n-button type="primary"> Primary </n-button>
+    <n-button type="info"> Info </n-button>
+    <n-button type="success"> Success </n-button>
+    <n-button type="warning"> Warning </n-button>
+    <n-button type="error"> Error </n-button>
   </div>
 </template>
+
+<script setup>
+import { NButton } from "naive-ui";
+</script>
--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import AutoImport from "unplugin-auto-import/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["nuxtjs-naive-ui"],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            "naive-ui": [
+              "useDialog",
+              "useMessage",
+              "useNotification",
+              "useLoadingBar",
+            ],
+          },
+        ],
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+    ],
+  },
+});
